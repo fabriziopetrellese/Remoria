b293c0b0468782792d2828b9d4d739cb844e3a39
valeriooconte <valeriodomenicoconte@gmail.com>
2022-05-24 14:53:22 +0200

    Initial Commit

---
+++ AlzHelper/AlzHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlzHelperApp.swift
+//  AlzHelper
+//
+//  Created by Valerio Domenico Conte on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AlzHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlzHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlzHelper
+//
+//  Created by Valerio Domenico Conte on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
