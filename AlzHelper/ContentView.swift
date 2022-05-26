98992adc4ff2aa029b80bc1e8926cedf0ef46dfa
Fabrizio Petrellese <petrellesefabrizio@icloud.com>
2022-05-26 15:03:26 +0200

    first github commit

--- AlzHelper/ContentView.swift
+++ AlzHelper/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                
+                NavigationLink {
+                    //                  CameraView()
+                } label: {
+                    ButtonView(title: "Take a photo")
+                }
+                
+                Text("or")
+                
+                NavigationLink {
+                    //                  GalleryView()
+                } label: {
+                    ButtonView(title: "Choose from library")
+                }
+                
+                
+            }
+            .navigationTitle("AlzHelper")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink {
+                        //                      InfoView()
+                    } label: {
+                        Image(systemName: "questionmark.circle")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ButtonView: View {
+    let title: String
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.orange)
+                .frame(width: 280, height: 80)
+            
+            Text(title)
+                .foregroundColor(.white)
+            
+        }
     }
 }
