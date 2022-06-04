f7a1d62908264e77a7195d9d75f2ddc0ca1ffb33
Simon Italia <simonitaliaatwork@gmail.com>
2022-06-04 13:19:54 +0200

    feature: Navigation - implement navigation to guess view

--- AlzHelper/ContentView.swift
+++ AlzHelper/ContentView.swift
@@ -8,63 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var navigationRoot = NavigationRoot()
+    @StateObject var navigationRoot = NavigationRoot()
     @EnvironmentObject var db: DatabaseDecoder
     @State var isView1Active = false
     @State var showModal: Bool = false
+    @State private var isGuessViewActive: Bool = false
     
     //camera view properties
     @State private var isCameraViewPresented: Bool = false
     @State private var selectedImage: UIImage?
     
     var body: some View {
-        if let selectedImage = selectedImage {
-            GuessView(
-//                itemName: "Some Object",
-//                itemImage: "Some Image",
-//                itemCategory: "Some Category",
-                itemUiImage: selectedImage
-            )
-            
-        } else {
-            NavigationView {
-                VStack {
-                    ButtonView(title: "Take a photo", icon: "camera.fill", color: .red)
-                        .onTapGesture {
-                            isCameraViewPresented.toggle()
-                        }
-                        .padding(.vertical)
-                    
-                    NavigationLink(destination: LibraryView(), isActive: $isView1Active) {
-                        ButtonView(title: "Choose from the Library", icon: "photo.on.rectangle.angled", color: .blue)
-                    }
-                    .isDetailLink(false)
-                    .padding(.vertical)
-                    
-                    NavigationLink {
-                    } label: {
-                        ButtonView(title: "Search by key-words", icon: "keyboard.fill", color: .yellow)
-                    }
-                    .padding(.vertical)
+        NavigationView {
+            VStack {
+                //Present Guess View
+                NavigationLink(
+                    destination: GuessView(itemUiImage:selectedImage),
+                    isActive: $isGuessViewActive
+                ) {}
+                
+                //Present Camera View
+                ButtonView(
+                    title: "Take a photo",
+                    icon: "camera.fill",
+                    color: .red
+                )
+                .onTapGesture {
+                    isCameraViewPresented.toggle()
                 }
-                .navigationTitle("AlzHelper")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button {
-                            showModal.toggle()
-                        } label: {
-                            Image(systemName: "questionmark.circle")
-                                .foregroundColor(.black)
-                        }
-                        .sheet(isPresented: $showModal) {
-                            OnboardingView(showModal: $showModal)
-                        }
-                    }
+                .padding(.vertical)
+                
+                //Present Library View
+                NavigationLink(
+                    destination: LibraryView()
+                        .environmentObject(navigationRoot),
+                    isActive: $isView1Active
+                ) {
+                    ButtonView(title: "Choose from the Library", icon: "photo.on.rectangle.angled", color: .blue)
                 }
+                .isDetailLink(false)
+                .padding(.vertical)
+                
+                NavigationLink {
+                } label: {
+                    ButtonView(title: "Search by key-words", icon: "keyboard.fill", color: .yellow)
+                }
+                .padding(.vertical)
             }
-            .fullScreenCover(isPresented: self.$isCameraViewPresented) {
-                ImagePickerView(selectedImage: self.$selectedImage, sourceType: .camera)
-                    .ignoresSafeArea()
+            .navigationTitle("Main Menu")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showModal.toggle()
+                    } label: {
+                        Image(systemName: "questionmark.circle")
+                            .foregroundColor(.black)
+                    }
+                    .sheet(isPresented: $showModal) {
+                        OnboardingView(showModal: $showModal)
+                    }
+                }
             }
             .onReceive(navigationRoot.$backToRoot) { moveToDashboard in
                 if moveToDashboard {
@@ -72,7 +75,17 @@ struct ContentView: View {
                     navigationRoot.backToRoot = false
                 }
             }
-            .environmentObject(navigationRoot)
+            .fullScreenCover(isPresented: self.$isCameraViewPresented) {
+                ImagePickerView(
+                    selectedImage: self.$selectedImage,
+                    sourceType: .camera
+                )
+                .ignoresSafeArea()
+            }
+        }
+        .onChange(of: selectedImage) { newValue in
+            guard newValue != nil else { return }
+            isGuessViewActive.toggle()
         }
     }
 }
